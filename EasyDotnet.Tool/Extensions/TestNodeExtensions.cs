4b04c5f036103fc7b45212e5c2be43507026a989
Gustav Eikaas <gustav.eikaas@gmail.com>
2025-05-16 16:37:00 +0200

    fix: handle missing namespaces for certain test adapters

--- EasyDotnet.Tool/Extensions/TestNodeExtensions.cs
+++ EasyDotnet.Tool/Extensions/TestNodeExtensions.cs
@@ -8,14 +8,14 @@ public static class TestNodeExtensions
   public static DiscoveredTest ToDiscoveredTest(this TestNodeUpdate test)
   {
     //TODO: I need to document this somewhere
-    var name = string.IsNullOrEmpty(test.Node.TestNamespace) ? test.Node.DisplayName : $"{test.Node.TestNamespace}.{test.Node.DisplayName}";
+    var name = string.IsNullOrEmpty(test.Node.TestNamespace) ? "root."+test.Node.DisplayName : $"{test.Node.TestNamespace}.{test.Node.DisplayName}";
     return new()
     {
       Id = test.Node.Uid,
       FilePath = test.Node?.FilePath?.Replace("\\", "/"),
       LineNumber = test.Node?.LineStart,
-      Namespace = test.Node.TestNamespace,
-      DisplayName = test.Node.DisplayName,
+      Namespace = test.Node?.TestNamespace,
+      DisplayName = test.Node?.DisplayName ?? "Unknown",
       Name = name
     };
   }
