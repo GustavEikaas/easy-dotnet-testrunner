6935ada0c31fd2a23bfdb16d5e0469c90e1e0d41
GustavEikaas <gustav.eikaas@gmail.com>
2025-06-13 21:33:06 +0200

    .

--- EasyDotnet.Tool/Controllers/MsBuild/DotnetProjectPropertiesResponse.cs
+++ EasyDotnet.Tool/Controllers/MsBuild/DotnetProjectPropertiesResponse.cs
@@ -22,22 +22,22 @@ public sealed record DotnetProjectPropertiesResponse(
 
 public static class DotnetProjectPropertiesExtensions
 {
-    public static DotnetProjectPropertiesResponse ToResponse(this DotnetProjectProperties props)
-        => new(
-            OutputPath: props.OutputPath,
-            OutputType: props.OutputType,
-            TargetExt: props.TargetExt,
-            AssemblyName: props.AssemblyName,
-            TargetFramework: props.TargetFramework,
-            TargetFrameworks: props.TargetFrameworks,
-            IsTestProject: props.IsTestProject,
-            UserSecretsId: props.UserSecretsId,
-            TestingPlatformDotnetTestSupport: props.TestingPlatformDotnetTestSupport,
-            TargetPath: props.TargetPath,
-            GeneratePackageOnBuild: props.GeneratePackageOnBuild,
-            IsPackable: props.IsPackable,
-            PackageId: props.PackageId,
-            Version: props.Version,
-            PackageOutputPath: props.PackageOutputPath
-        );
+  public static DotnetProjectPropertiesResponse ToResponse(this DotnetProjectProperties props)
+      => new(
+          OutputPath: props.OutputPath,
+          OutputType: props.OutputType,
+          TargetExt: props.TargetExt,
+          AssemblyName: props.AssemblyName,
+          TargetFramework: props.TargetFramework,
+          TargetFrameworks: props.TargetFrameworks,
+          IsTestProject: props.IsTestProject,
+          UserSecretsId: props.UserSecretsId,
+          TestingPlatformDotnetTestSupport: props.TestingPlatformDotnetTestSupport,
+          TargetPath: props.TargetPath,
+          GeneratePackageOnBuild: props.GeneratePackageOnBuild,
+          IsPackable: props.IsPackable,
+          PackageId: props.PackageId,
+          Version: props.Version,
+          PackageOutputPath: props.PackageOutputPath
+      );
 }
