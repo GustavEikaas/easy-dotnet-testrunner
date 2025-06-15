6e578d2c000448bc9cd41a5c3a9d5200c690497a
GustavEikaas <gustav.eikaas@gmail.com>
2025-06-15 07:13:48 +0200

    cleanup

--- EasyDotnet.Tool/Services/NotificationService.cs
+++ EasyDotnet.Tool/Services/NotificationService.cs
@@ -3,21 +3,9 @@ using StreamJsonRpc;
 
 namespace EasyDotnet.Services;
 
-// public enum Job
-// {
-//   restore = 0,
-//   build = 1
-// }
-
-// public sealed record JobStartNotification(Guid Id, Job Job, string Message, object? Detail = null);
-// public sealed record JobEndedNotification(Guid Id, Job Job, string Message, bool Success, string[]? Errors, object? Detail = null);
-// public sealed record JobEndedArgument(bool Success, string[]? Errors, object? Detail = null);
 public sealed record ServerRestoreRequest(string TargetPath);
 
 public class NotificationService(JsonRpc jsonRpc)
 {
   public async Task RequestRestoreAsync(string targetPath) => await jsonRpc.NotifyWithParameterObjectAsync("request/restore", new ServerRestoreRequest(targetPath));
-
-  // public async Task NotifyJobStartAsync(Guid id, Job job, string message, object? detail = null) => await jsonRpc.NotifyWithParameterObjectAsync("job/started", new JobStartNotification(id, job, message, detail));
-  // public async Task NotifyJobEndAsync(Guid id, Job job, string message, JobEndedArgument arg) => await jsonRpc.NotifyAsync("job/ended", new JobEndedNotification(id, job, message, arg.Success, arg?.Errors, arg?.Detail));
 }
