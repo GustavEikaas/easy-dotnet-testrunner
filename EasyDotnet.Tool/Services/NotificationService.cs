208318238c6d9bfff1b94c2c6fd289017a05a879
GustavEikaas <gustav.eikaas@gmail.com>
2025-06-15 07:40:24 +0200

    format

--- EasyDotnet.Tool/Services/NotificationService.cs
+++ EasyDotnet.Tool/Services/NotificationService.cs
@@ -7,7 +7,7 @@ namespace EasyDotnet.Services;
 public sealed record ServerRestoreRequest(string TargetPath);
 //marker interface
 public interface INotificationService { }
-public class NotificationService(JsonRpc jsonRpc): INotificationService
+public class NotificationService(JsonRpc jsonRpc) : INotificationService
 {
   [RpcNotification("request/restore")]
   public async Task RequestRestoreAsync(string targetPath) => await jsonRpc.NotifyWithParameterObjectAsync("request/restore", new ServerRestoreRequest(targetPath));
