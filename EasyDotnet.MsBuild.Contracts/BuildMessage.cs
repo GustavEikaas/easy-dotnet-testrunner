d0103a4dca4c4cfc3373f93cafccb30775fc1902
GustavEikaas <gustav.eikaas@gmail.com>
2025-06-24 19:13:18 +0200

    .

--- EasyDotnet.MsBuild.Contracts/BuildMessage.cs
+++ EasyDotnet.MsBuild.Contracts/BuildMessage.cs
@@ -1,22 +1,22 @@
 namespace EasyDotnet.MsBuild.Contracts
 {
-    public sealed class BuildMessage
-    {
-        public string Type { get; }
-        public string FilePath { get; }
-        public int LineNumber { get; }
-        public int ColumnNumber { get; }
-        public string Code { get; }
-        public string Message { get; }
+  public sealed class BuildMessage
+  {
+    public string Type { get; }
+    public string FilePath { get; }
+    public int LineNumber { get; }
+    public int ColumnNumber { get; }
+    public string Code { get; }
+    public string Message { get; }
 
-        public BuildMessage(string type, string filePath, int lineNumber, int columnNumber, string code, string message)
-        {
-            Type = type;
-            FilePath = filePath;
-            LineNumber = lineNumber;
-            ColumnNumber = columnNumber;
-            Code = code;
-            Message = message;
-        }
+    public BuildMessage(string type, string filePath, int lineNumber, int columnNumber, string code, string message)
+    {
+      Type = type;
+      FilePath = filePath;
+      LineNumber = lineNumber;
+      ColumnNumber = columnNumber;
+      Code = code;
+      Message = message;
     }
+  }
 }
--- EasyDotnet.MsBuild.Contracts/BuildResult.cs
+++ EasyDotnet.MsBuild.Contracts/BuildResult.cs
@@ -2,17 +2,17 @@ using System.Collections.Generic;
 
 namespace EasyDotnet.MsBuild.Contracts
 {
-    public sealed class BuildResult
-    {
-        public bool Success { get; }
-        public List<BuildMessage> Errors { get; }
-        public List<BuildMessage> Warnings { get; }
+  public sealed class BuildResult
+  {
+    public bool Success { get; }
+    public List<BuildMessage> Errors { get; }
+    public List<BuildMessage> Warnings { get; }
 
-        public BuildResult(bool success, List<BuildMessage> errors, List<BuildMessage> warnings)
-        {
-            Success = success;
-            Errors = errors ?? new List<BuildMessage>();
-            Warnings = warnings ?? new List<BuildMessage>();
-        }
+    public BuildResult(bool success, List<BuildMessage> errors, List<BuildMessage> warnings)
+    {
+      Success = success;
+      Errors = errors ?? new List<BuildMessage>();
+      Warnings = warnings ?? new List<BuildMessage>();
     }
+  }
 }
